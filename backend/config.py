93468d4cb4384d78930c4216bd2078d4e6c1cd82
rickhongliu <ladyrick@qq.com>
2018-07-07 17:13:19 +0800

    add check_same_thread for sqlite3

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,4 @@ from sqlalchemy import create_engine
 DEBUG = True
 
 # database engine. Use sqlite3 for debug
-engine = create_engine('sqlite:///foo.db', echo=DEBUG)
+engine = create_engine('sqlite:///foo.db?check_same_thread=False', echo=DEBUG)
