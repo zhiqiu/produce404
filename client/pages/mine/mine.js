abd986f6c8506e67442f473a24af646d440adca7
Jayden1994 <chengdeng1994@hotmail.com>
2018-07-11 19:29:55 +0800

    修改“我的”界面从后台读取

--- client/pages/mine/mine.js
+++ client/pages/mine/mine.js
@@ -6,80 +6,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    user: {},
+    feeds: {},
+    medal: {},
+    userMsg: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    this.setData({
+      user: getApp().globalData.myData.userInfo.resp.user,
+      feeds: getApp().globalData.myData.feeds,
+      medal: getApp().globalData.myData.medal,
+    }),
+    console.log(this.data)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   },
 
-  gotoMyMsg: function () {
+  gotoMyMsg: function() {
     var audioInfo = ''
     wx.navigateTo({
       url: '/pages/mine/my_msg/my_msg?' + audioInfo
     })
   },
 
-  gotoMyColl: function () {
+  gotoMyColl: function() {
     var audioInfo = ''
     wx.navigateTo({
       url: '/pages/mine/my_coll/my_coll?' + audioInfo
     })
   },
 
-  gotoMyAgreement: function () {
+  gotoMyAgreement: function() {
     var audioInfo = ''
     wx.navigateTo({
       url: '/pages/mine/my_agreement/my_agreement?' + audioInfo
