378fdad80a0827635faa0829a87223a31fcff67b
ladyrick <ladyrick@qq.com>
2018-07-12 01:17:26 +0800

    use qcloud

--- backend/config.py
+++ backend/config.py
@@ -17,7 +17,8 @@ databaseFile = os.path.join(curdir, "..", "foo.db")
 # engine = create_engine("sqlite:///%s?check_same_thread=False" % databaseFile, echo=False)
 
 # use mysql for production environment.
-engine = create_engine("mysql+pymysql://dba:create404mysql@ladyrick.com/create404?charset=utf8", pool_recycle=3600, echo=False)
+engine = create_engine("mysql+pymysql://root:create404mysql@172.16.16.7/create404?charset=utf8mb4", pool_recycle=3600, echo=False)
+# engine = create_engine("mysql+pymysql://dba:create404mysql@ladyrick.com/create404?charset=utf8", pool_recycle=3600, echo=False)
 
 # app config
 appID = "appID"
