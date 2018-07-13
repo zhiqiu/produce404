d7337f10e22ca4a65e692cdc84b1ffdfc7e0c31b
zhiqiu <izhiqiu@foxmail.com>
2018-07-13 19:58:49 +0800

    update gunicorn -w 3 for three workers

--- backend/run.sh
+++ backend/run.sh
@@ -7,6 +7,6 @@ ps aux | grep "gunicorn" |grep -v grep| cut -c 9-15 |xargs -r kill -9
 echo 'ok'
 
 #/usr/local/python3/bin/gunicorn -D -w 2 -t 90 -k 'gevent' -b 127.0.0.1:8080 wsgi:app
-/usr/local/python3/bin/gunicorn -D -t 90 -b 127.0.0.1:8080 wsgi:app
+/usr/local/python3/bin/gunicorn -D -w 3 -t 90 -b 127.0.0.1:8080 wsgi:app
 
 ps -aux | grep gunicorn
