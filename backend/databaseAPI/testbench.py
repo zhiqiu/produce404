e0d0f4678d50aa4b46ca6bb395c20bf3c8cd3ed7
ladyrick <ladyrick@qq.com>
2018-07-14 15:48:28 +0800

    add audio tag for every audio

--- backend/databaseAPI/testbench.py
+++ backend/databaseAPI/testbench.py
@@ -56,10 +56,10 @@ def makeTestDatabase(session):
             "audio_id": i,
         }).create(session)
 
-    for i in range(1, 10):
+    for i in range(1, 100):
         R_Audio_Has_AudioTag(**{
             "audio_id": i,
-            "audiotag_id": i,
+            "audiotag_id": i % 9 + 1,
         }).create(session)
 
     for i in range(1, 10):
